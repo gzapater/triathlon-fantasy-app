0667556395f0b57361795f19626ac9c49d0fc023
gzapater <90698513+gzapater@users.noreply.github.com>
2025-06-02 01:02:05 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageArea = document.getElementById('messageArea');
 
     helloButton.addEventListener('click', () => {
-        fetch('http://13.49.77.162/api/hello')
+        fetch('https://triathlon-fantasy.duckdns.org/api/hello')
             .then(response => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
