4e956e2f78e8b6b63aa8a707fd20ed91163dedd4
gzapater <90698513+gzapater@users.noreply.github.com>
2025-06-01 23:37:51 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def hello():
     return jsonify(message="Hello World from Backend!")
 
+# Esta parte solo se ejecuta si corres el script directamente (ej. python app.py)
+# Gunicorn no la ejecutará cuando importe 'app:app'
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True) # Añadido debug=True para desarrollo local
