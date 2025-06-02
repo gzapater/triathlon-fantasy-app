82b622b939e815742d1acb0e001edafeef12772b
gzapater <90698513+gzapater@users.noreply.github.com>
2025-06-03 00:22:41 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -155,8 +155,8 @@ def serve_login_js():
     return send_from_directory('../frontend', 'login.js')
 
 @app.route('/register.js')
-def serve_register_js():More actions
-    return send_from_directory('../frontend', 'register.js')
+def serve_register_js():
+    return send_from_directory('../frontend', 'register.js')More actions
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
