d8b088de057495cefb2384a2ca244a67d39f1078
gzapater <90698513+gzapater@users.noreply.github.com>
2025-06-03 11:44:49 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,8 @@ db = SQLAlchemy()
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    code = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=False) 
 
     def __repr__(self):
         return f'<Role {self.name}>'
