ce92d405e7d7e028e8b26addb3d7ae9e1a8e9920
Ubuntu <ubuntu@ip-172-31-20-172.eu-north-1.compute.internal>
2025-06-24 09:35:42 +0000

    Fix: Merge multiple Alembic heads in main

---
+++ backend/migrations/versions/1b4852e9d906_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1b4852e9d906
+Revises: 3c420d1b9335, abcdef123456
+Create Date: 2025-06-24 09:34:39.528987
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1b4852e9d906'
+down_revision = ('3c420d1b9335', 'abcdef123456')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
